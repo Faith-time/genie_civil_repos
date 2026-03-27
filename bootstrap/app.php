d32de93dd99c6bb4f8537e982d3a1efd0e04edec
Fatime <mamanfatim2017@gmail.com>
2026-03-27 23:34:31 +0000

    fix: trust proxies for HTTPS behind Render/Cloudflare

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Alias des middlewares
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             // JWT pour API uniquement
             'jwt.auth' => JwtMiddleware::class,
